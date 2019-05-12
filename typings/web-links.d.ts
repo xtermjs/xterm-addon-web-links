192e13b406275d071aed418c97ab6e1db475e335
Daniel Imms <daimms@microsoft.com>
2019-05-12 11:34:20 -0700

    Add jsdoc to API
    Fixes #3

--- typings/web-links.d.ts
+++ typings/web-links.d.ts
@@ -4,17 +4,32 @@
  */
 
 
-import { Terminal, ILinkMatcherOptions } from 'xterm';
+import { Terminal, IDisposable, ILinkMatcherOptions } from 'xterm';
 
 // TODO: This is temporary, link to xterm when the new version is published
-export interface ITerminalAddon {
+export interface ITerminalAddon extends IDisposable {
   activate(terminal: Terminal): void;
-  dispose(): void;
 }
 
+/**
+ * An xterm.js addon that enables web links.
+ */
 export class WebLinksAddon implements ITerminalAddon {
+  /**
+   * Creates a new web links addon.
+   * @param handler The callback when the link is called.
+   * @param options Options for the link matcher.
+   */
   constructor(handler?: (event: MouseEvent, uri: string) => void, options?: ILinkMatcherOptions);
 
+  /**
+   * Activates the addon
+   * @param terminal The terminal the addon is being loaded in.
+   */
   public activate(terminal: Terminal): void;
+
+  /**
+   * Disposes the addon.
+   */
   public dispose(): void;
 }
