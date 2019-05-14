88480affdfdbaffe3de2671a5d10b0bb549ce638
Daniel Imms <daimms@microsoft.com>
2019-05-14 10:43:37 -0700

    Await in after step
    Fixes #13

--- src/WebLinksAddon.test.ts
+++ src/WebLinksAddon.test.ts
@@ -26,8 +26,8 @@ describe('API Integration Tests', () => {
     await page.setViewport({ width, height });
   });
 
-  after(() => {
-    browser.close();
+  after(async () => {
+    await browser.close();
   });
 
   beforeEach(async function(): Promise<any> {
